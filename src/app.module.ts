93c5e2e13304f257842a27a67cf98dce9830fed7
Ruan_Bertin <ruanbertin@hotmail.com>
2024-04-04 22:35:09 -0300

    fix: corrigindo conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ShelterModule } from './shelter/shelter.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ShelterModule, MongooseModule.forRoot('mongodb+srv://RuanBertin:Mugiwara2807@piclescluster.1n40fit.mongodb.net/picles?retryWrites=true&w=majority&appName=PiclesCluster')],
+  imports: [ShelterModule, MongooseModule.forRoot('')],
   controllers: [AppController],
   providers: [AppService],
 })
