53755ad3b2cbf346837abd0d35336056e408c5e5
Ruan_Bertin <ruanbertin@hotmail.com>
2024-04-05 22:34:01 -0300

    feat: Criando getShelterFlow e updateController

--- src/shelter/shelter.tokens.ts
+++ src/shelter/shelter.tokens.ts
@@ -1,3 +1,6 @@
 export default class ShelterTokens {
-    static getShelterDetailsUseCase = "getShelterDetailsUseCase"
+    static getShelterDetailsUseCase = "getShelterDetailsUseCase";
+    static updateShelterDetailsUseCase = "updatetShelterDetailsUseCase";
+
+    static shelterRepository = 'shelterRepository';
 }
--- src/shelter/usecases/get.shelter.details.usecase.ts
+++ src/shelter/usecases/get.shelter.details.usecase.ts
@@ -1,15 +1,25 @@
 import { IUseCase } from "src/domain/iusecase.interface";
-import GetShelterDetailsUseCaseOutput from "../dtos/get.shelter.details.usecase.output";
+import GetShelterDetailsUseCaseOutput from "./dtos/get.shelter.details.usecase.output";
+import IShelterRepository from "../interfaces/shelter.repository.interface";
+import { Inject } from "@nestjs/common";
+import ShelterTokens from "../shelter.tokens";
 
 export default class GetShelterDetailsUseCase implements IUseCase<null, GetShelterDetailsUseCaseOutput>{
-    run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
-        return Promise.resolve(new GetShelterDetailsUseCaseOutput({
-            shelterName: 'Abrigo Bigo',
-            shelterEmail: 'ab@gmail.com',
-            shelterPhone: '19981281830',
-            shelterWhatsApp: '19981281830',
-            createdAt: new Date(),
-            updateAt: new Date()
-        }))
+    constructor(
+        @Inject(ShelterTokens.shelterRepository)
+        private readonly shelterRepository: IShelterRepository,
+    ){}
+    
+    async run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
+        const shelter = await this.shelterRepository.get();
+
+        return new GetShelterDetailsUseCaseOutput({
+            shelterName: shelter.name,
+            shelterEmail: shelter.email,
+            shelterPhone: shelter.phone,
+            shelterWhatsApp: shelter.whatsApp,
+            createdAt: shelter.createdAt,
+            updateAt: shelter.updateAt,
+        });
     }
 }
