c4b840c02f12b0d5d78043f43110cc26abefeea8
Ruan_Bertin <ruanbertin@hotmail.com>
2024-04-11 22:30:39 -0300

    feat: Criando fluxo de obtenção de pets

--- src/pet/usecases/update.pet.by.id.usecase.ts
+++ src/pet/usecases/update.pet.by.id.usecase.ts
@@ -6,6 +6,8 @@ import PetTokens from "../pet.tokens";
 import IPetRepository from "../interfaces/pet.repository.interface";
 import { Pet } from "../schemas/pet.schema";
 import PetNotFoundError from "src/domain/errors/pet.not.found.error";
+import AppTokens from "src/app.tokens";
+import IFileService from "src/interfaces/file.service.interface";
 
 
 @Injectable()
@@ -13,7 +15,10 @@ export default class UpdatePetByIdUseCase implements IUseCase<UpdatePetByIdUseCa
 
     constructor(
         @Inject(PetTokens.petRepository)
-        private readonly petRepository: IPetRepository
+        private readonly petRepository: IPetRepository,
+
+        @Inject(AppTokens.fileService)
+        private readonly filseService: IFileService
     ) { }
 
     async run(input: UpdatePetByIdUseCaseInput): Promise<UpdatePetByIdUseCaseOutput> {
@@ -23,13 +28,15 @@ export default class UpdatePetByIdUseCase implements IUseCase<UpdatePetByIdUseCa
             throw new PetNotFoundError()
         }
 
+        
         await this.petRepository.updateById({
             ...input,
             _id: input.id
         });
-
+        
         pet = await this.getPetById(input.id)
-
+        
+        const petPhoto = !!pet.photo ? (await this.filseService.readFile(pet.photo)).toString('base64') : null;
         return new UpdatePetByIdUseCaseOutput({
             id: pet._id,
             name: pet.name,
